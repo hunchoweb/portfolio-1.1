f39bf7b73ecb05afd71450716c5b7904616a7a51
Ojowa Odunayo <hunchoweb@gmail.com>
2023-09-13 18:58:39 +0100

    updated sanity

--- sanity/schemas/skill.js
+++ sanity/schemas/skill.js
@@ -13,7 +13,6 @@ export default {
       name: "progress",
       title: "Progres",
       type: "number",
-      description: "Progress of skill from 0 to 100%",
       validation: (Rule) => Rule.min(0).max(100),
     },
     {
